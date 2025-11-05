5a70926c0a5b5970a617a7778a8263c71b52fb07
Ross Tyler <rossetyler@gmail.com>
2025-11-05 12:13:58 -0800

    simplify raii.hpp to use std::unique_ptr with no move constructors

--- components/_smtp/raii.hpp
+++ components/_smtp/raii.hpp
@@ -1,59 +1,32 @@
 #pragma once
 
-#include <concepts>
-#include <type_traits>
-#include <utility>
-#include <functional>
-#include <stdexcept>
+#include <memory>
 
 namespace raii {
 
 // Implements RAII for resources with unary acquisition and release functions.
-// The acquirer is called with a pointer to the default-constructed resource.
-// The acquirer must throw on failure to ensure the releaser is only called
-// on a valid resource during destruction.
+// The acquirer is called with a pointer to the default-constructed encapsulated resource.
+// The releaser will not be called if the acquirer throws an exception.
+// implicit accessor operators are provided to the encapsulated resource
+// as well as explicit get methods.
 template<typename Resource, typename Acquirer, typename Releaser>
   requires std::default_initializable<Resource>
 class Raii {
  private:
   Resource resource_;
-  Releaser releaser_;
-  bool valid_;
+  std::unique_ptr<Resource, Releaser> releaser_;
 
  public:
-  Raii(Acquirer &&acquirer, Releaser &&releaser) : resource_{}, releaser_(std::move(releaser)), valid_(true) {
-    acquirer(&this->resource_);
-  }
+  Raii(Acquirer &&acquirer, Releaser &&releaser)
+    : resource_{}
+    , releaser_{(acquirer(&resource_), &resource_), std::move(releaser)}
+  {}
 
-  ~Raii() { this->release(); }
-
-  void release() {
-    if (this->valid_) {
-      this->releaser_(&this->resource_);
-      this->valid_ = false;
-    }
-  }
-
+  // delete unsafe default copy and move constructors
   Raii(const Raii &) = delete;
   Raii &operator=(const Raii &) = delete;
-
-  Raii(Raii &&that) noexcept
-      : resource_(std::move(that.resource_)), releaser_(std::move(that.releaser_)), valid_(that.valid_) {
-    that.valid_ = false;
-  }
-
-  Raii &operator=(Raii &&that) noexcept {
-    if (this != &that) {
-      if (this->valid_) {
-        this->releaser_(this->resource_);
-      }
-      this->resource_ = std::move(that.resource_);
-      this->releaser_ = std::move(that.releaser_);
-      this->valid_ = that.valid_;
-      that.valid_ = false;
-    }
-    return *this;
-  }
+  Raii(Raii &&that) = delete;
+  Raii &operator=(Raii &&that) = delete;
 
   template<typename R = Resource>
     requires(!std::is_pointer_v<R>)
@@ -89,19 +62,32 @@ class Raii {
   const Resource &get() const { return this->resource_; }
 };
 
-// Helper to extract T from unary function pointer void (*)(T*)
-template<typename F> struct UnaryFunctionArg;
+template<typename R>
+struct UnaryFunctionArg;
 
-template<typename T> struct UnaryFunctionArg<void (*)(T *)> {
-  using Type = T;
+template<typename R, typename T>
+struct UnaryFunctionArg<R(*)(T)> {
+  using type = std::remove_pointer_t<T>;
 };
 
-template<typename Acquirer, typename Releaser> auto make(Acquirer &&acquirer, Releaser &&releaser) {
-  using T = typename UnaryFunctionArg<std::decay_t<Acquirer>>::Type;
-  return Raii<T, std::decay_t<Acquirer>, std::decay_t<Releaser>>(std::forward<Acquirer>(acquirer),
-                                                                 std::forward<Releaser>(releaser));
+template<typename R, typename T>
+struct UnaryFunctionArg<R(&)(T)> {
+  using type = std::remove_pointer_t<T>;
+};
+
+// make an Raii with automatic Resource type deduction from acquirer's unary argument
+template<typename Acquirer, typename Releaser>
+auto make(Acquirer&& acquirer, Releaser&& releaser) {
+    using F = decltype(+std::forward<Acquirer>(acquirer));  // + forces pointer
+    using T = typename UnaryFunctionArg<F>::type;
+    return Raii<T, std::decay_t<Acquirer>, std::decay_t<Releaser>>{
+        std::forward<Acquirer>(acquirer),
+        std::forward<Releaser>(releaser)
+    };
 }
 
-template<typename T> using Resource = decltype(make(std::declval<void (*)(T *)>(), std::declval<void (*)(T *)>()));
+// alias of the Resource type from an Raii we could make from an acquirer
+template<typename T>
+using Resource = decltype(make(std::declval<void(*)(T*)>(), std::declval<void(*)(T*)>()));
 
 }  // namespace raii
