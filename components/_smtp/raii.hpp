6a1832c6b2962944336e577fd16a2baaa00e2b90
Ross Tyler <rossetyler@gmail.com>
2025-11-06 11:50:59 -0800

    smtp raii cleanup

--- components/_smtp/raii.hpp
+++ components/_smtp/raii.hpp
@@ -1,26 +1,27 @@
 #pragma once
 
+#include <functional>
 #include <memory>
 
 namespace raii {
 
-// Implements RAII for resources with unary acquisition and release functions.
-// The acquirer is called with a pointer to the default-constructed encapsulated resource.
-// The releaser will not be called if the acquirer throws an exception.
-// implicit accessor operators are provided to the encapsulated resource
+// Raii implements RAII for a Resource with unary Acquirer and Releaser callables
+// that will be invoked with a pointer its default_initializable Resource member.
+// The Acquirer is invoked upon construction and, if successful, the Releaser
+// will be invoked upon destruction.
+// Implicit accessor operators are provided to the protected Resource member
 // as well as explicit get methods.
-template<typename Resource, typename Acquirer, typename Releaser>
+template<typename Resource, typename Acquirer = void (*)(Resource *), typename Releaser = void (*)(Resource *)>
   requires std::default_initializable<Resource>
 class Raii {
- private:
+ protected:
   Resource resource_;
   std::unique_ptr<Resource, Releaser> releaser_;
 
  public:
   Raii(Acquirer &&acquirer, Releaser &&releaser)
-    : resource_{}
-    , releaser_{(acquirer(&resource_), &resource_), std::move(releaser)}
-  {}
+      : resource_{},
+        releaser_{(std::invoke(std::forward<Acquirer>(acquirer), &resource_), &resource_), std::move(releaser)} {}
 
   // delete unsafe default copy and move constructors
   Raii(const Raii &) = delete;
@@ -28,66 +29,72 @@ class Raii {
   Raii(Raii &&that) = delete;
   Raii &operator=(Raii &&that) = delete;
 
-  template<typename R = Resource>
-    requires(!std::is_pointer_v<R>)
-  operator R &() {
-    return this->resource_;
+  // non-pointer Resource: act like a reference
+  operator Resource &()
+    requires(!std::is_pointer_v<Resource>)
+  {
+    return resource_;
+  }
+  operator const Resource &() const
+    requires(!std::is_pointer_v<Resource>)
+  {
+    return resource_;
+  }
+  operator Resource *()
+    requires(!std::is_pointer_v<Resource>)
+  {
+    return &resource_;
   }
 
-  template<typename R = Resource>
-    requires(!std::is_pointer_v<R>)
-  operator const R &() const {
-    return this->resource_;
+  // pointer Resource: smart-pointer-like
+  auto operator->() const
+    requires std::is_pointer_v<Resource>
+  {
+    return resource_;
   }
-
-  template<typename R = Resource>
-    requires(!std::is_pointer_v<R>)
-  operator R *() {
-    return &this->resource_;
-  }
-
-  template<typename R = Resource>
-    requires std::is_pointer_v<R>
-  auto operator->() const {
-    return this->resource_;
-  }
-
-  template<typename R = Resource>
-    requires std::is_pointer_v<R>
-  decltype(auto) operator*() const {
-    return *this->resource_;
+  decltype(auto) operator*() const
+    requires std::is_pointer_v<Resource>
+  {
+    return *resource_;
   }
 
   Resource &get() { return this->resource_; }
   const Resource &get() const { return this->resource_; }
 };
 
-template<typename R>
-struct UnaryFunctionArg;
+// ---- generic unary callable argument type deduction
 
-template<typename R, typename T>
-struct UnaryFunctionArg<R(*)(T)> {
-  using type = std::remove_pointer_t<T>;
+// primary template for specializations (the base for SFINAE)
+template<typename Callable> struct deduce_argument;
+
+// mutable lambda or non-const (default) function object
+template<typename R, typename C, typename A> struct deduce_argument<R (C::*)(A)> {
+  using type = std::remove_pointer_t<A>;
 };
 
-template<typename R, typename T>
-struct UnaryFunctionArg<R(&)(T)> {
-  using type = std::remove_pointer_t<T>;
+// immutable (default) lambda or const function object
+template<typename R, typename C, typename A> struct deduce_argument<R (C::*)(A) const> {
+  using type = std::remove_pointer_t<A>;
 };
 
-// make an Raii with automatic Resource type deduction from acquirer's unary argument
-template<typename Acquirer, typename Releaser>
-auto make(Acquirer&& acquirer, Releaser&& releaser) {
-    using F = decltype(+std::forward<Acquirer>(acquirer));  // + forces pointer
-    using T = typename UnaryFunctionArg<F>::type;
-    return Raii<T, std::decay_t<Acquirer>, std::decay_t<Releaser>>{
-        std::forward<Acquirer>(acquirer),
-        std::forward<Releaser>(releaser)
-    };
+// function pointer (function references decay to this)
+template<typename R, typename A> struct deduce_argument<R (*)(A)> {
+  using type = std::remove_pointer_t<A>;
+};
+
+// Callable lambda or function object, deduce_argument from what it decays to
+template<typename Callable> struct deduce_argument {
+  using type = typename deduce_argument<decltype(&std::decay_t<Callable>::operator())>::type;
+};
+
+// Callable function, deduce_argument from what it decays to
+template<typename Callable> using deduce_argument_t = typename deduce_argument<std::decay_t<Callable>>::type;
+
+// factory
+template<typename Acquirer, typename Releaser> auto make(Acquirer &&acquirer, Releaser &&releaser) {
+  using Resource = deduce_argument_t<Acquirer>;
+  return Raii<Resource, std::decay_t<Acquirer>, std::decay_t<Releaser>>{std::forward<Acquirer>(acquirer),
+                                                                        std::forward<Releaser>(releaser)};
 }
-
-// alias of the Resource type from an Raii we could make from an acquirer
-template<typename T>
-using Resource = decltype(make(std::declval<void(*)(T*)>(), std::declval<void(*)(T*)>()));
 
 }  // namespace raii
